e6c65bb0a5e379bc9803de07f8ca210204d2ea91
wangcy90 <chongyangfly@gmail.com>
2016-06-07 16:37:04 +0800

    add author infomation

--- SegmentPagerController/main.m
+++ SegmentPagerController/main.m
@@ -2,6 +2,11 @@
 //  main.m
 //  SegmentPagerController
 //
+//  email：chongyangfly@163.com
+//  QQ：1909295866
+//  github：https://github.com/wangcy90
+//  blog：http://wangcy90.github.io
+//
 //  Created by WangChongyang on 16/6/7.
 //  Copyright © 2016年 WangChongyang. All rights reserved.
 //
